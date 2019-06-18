90a7c3771d628a2b3615e8b8d5e13b491ea95813
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-06-17 23:12:52 -0500

    Change operands to comply with ECMA

--- js/modules/jquery.easing.js
+++ js/modules/jquery.easing.js
@@ -95,13 +95,13 @@ jQuery.extend(jQuery.easing,
       return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b
     },
     easeInExpo: function (x, t, b, c, d) {
-      return (t == 0) ? b : c * Math.pow(2, 10 * (t / d - 1)) + b
+      return (t === 0) ? b : c * Math.pow(2, 10 * (t / d - 1)) + b
     },
     easeOutExpo: function (x, t, b, c, d) {
       return (t == d) ? b + c : c * (-Math.pow(2, -10 * t / d) + 1) + b
     },
     easeInOutExpo: function (x, t, b, c, d) {
-      if (t == 0) return b
+      if (t === 0) return b
       if (t == d) return b + c
       if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b
       return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b
@@ -124,7 +124,7 @@ jQuery.extend(jQuery.easing,
     },
     easeOutElastic: function (x, t, b, c, d) {
       var s = 1.70158; var p = 0; var a = c
-      if (t == 0) return b; if ((t /= d) == 1) return b + c; if (!p) p = d * 0.3
+      if (t === 0) return b; if ((t /= d) == 1) return b + c; if (!p) p = d * 0.3
       if (a < Math.abs(c)) { a = c; var s = p / 4 } else var s = p / (2 * Math.PI) * Math.asin(c / a)
       return a * Math.pow(2, -10 * t) * Math.sin((t * d - s) * (2 * Math.PI) / p) + c + b
     },
