5d31d400834b29bb088b107c29102807058b53b3
Adolf97 <ramirez.desanc@gmail.com>
2019-06-24 20:32:22 -0500

    counter js

--- reservar/js/counter.js
+++ reservar/js/counter.js
@@ -3,25 +3,31 @@ var hdPrice = 1500;
 var hdlPrice = 1800;
 var hfPrice = 2000;
 
+var hsAva = 6;
+var hdAva = 6;
+var hdlAva = 6;
+var hfAva = 6;
+
 //Hab Sencilla
 var i = 0;
 
 function clickLinkHS() {
     i++;
-    if (i == 1) {
+    if (i <= 6) {
         document.getElementById("clicks-hs").innerHTML = i;
     } else {
+        i = 6;
+        
         document.getElementById("clicks-hs").innerHTML = i;
     }
 }
 
-function quitarHS() {
-    if (i <= 0) {
-        i = 0;
-    } else {
-        i--;
-        document.getElementById("clicks-hs").innerHTML = i;
-    }
+function resetHS() {
+    i = 0;
+    hsAva = 6;
+    
+    document.getElementById("clicks-hs").innerHTML = i;
+    document.getElementById("clicks-ava-hs").innerHTML = hsAva;
 }
 
 //Hab doble
@@ -29,20 +35,21 @@ var j = 0;
 
 function clickLinkHD() {
     j++;
-    if (j == 1) {
+    if (j <= 6) {
         document.getElementById("clicks-hd").innerHTML = j;
     } else {
+        j = 6;
+        
         document.getElementById("clicks-hd").innerHTML = j;
     }
 }
 
-function quitarHD() {
-    if (j <= 0) {
-        j = 0;
-    } else {
-        j--;
-        document.getElementById("clicks-hd").innerHTML = j;
-    }
+function resetHD() {
+    j = 0;
+    hdAva = 6;
+    
+    document.getElementById("clicks-hd").innerHTML = j;
+    document.getElementById("clicks-ava-hd").innerHTML = hdAva;
 }
 
 //Hab de lujo
@@ -50,20 +57,21 @@ var k = 0;
 
 function clickLinkHDL() {
     k++;
-    if (k == 1) {
+    if (k <= 6) {
         document.getElementById("clicks-hdl").innerHTML = k;
     } else {
+        k = 6;
+        
         document.getElementById("clicks-hdl").innerHTML = k;
-      }
+    }
 }
 
-function quitarHDL() {
-      if (k <= 0) {
-          k = 0;
-      } else {
-          k--;
-          document.getElementById("clicks-hdl").innerHTML = k;
-      }
+function resetHDL() {
+    k = 0;
+    hdlAva = 6;
+    
+    document.getElementById("clicks-hdl").innerHTML = k;
+    document.getElementById("clicks-ava-hdl").innerHTML = hdlAva;
 }
 
 //Hab Familiar
@@ -71,20 +79,21 @@ var l = 0;
 
 function clickLinkHF() {
     l++;
-    if (l == 1) {
+    if (l <= 6) {
         document.getElementById("clicks-hf").innerHTML = l;
     } else {
+        l = 6;
+        
         document.getElementById("clicks-hf").innerHTML = l;
     }
 }
 
-function quitarHF() {
-    if (l <= 0) {
-        l = 0;
-    } else {
-        l--;
-        document.getElementById("clicks-hf").innerHTML = l;
-    }
+function resetHF() {
+    l = 0;
+    hfAva = 6;
+    
+    document.getElementById("clicks-hf").innerHTML = l;
+    document.getElementById("clicks-ava-hf").innerHTML = hfAva;
 }
 
 //Price Algorithm
@@ -96,5 +105,49 @@ function pagar() {
         document.getElementById("clicks-price").innerHTML = m;
     } else {
         document.getElementById("clicks-price").innerHTML = m;
+    }
+}
+
+//Available Hab Sencilla
+function pagarHS() {
+    n = hsAva - i;
+    
+    if (n <= 0) {
+        document.getElementById("clicks-ava-hs").innerHTML = 0;
+    } else {
+        document.getElementById("clicks-ava-hs").innerHTML = n;
+    }
+}
+
+//Available Hab Doble
+function pagarHD() {
+    o = hdAva - j;
+    
+    if (o <= 0) {
+        document.getElementById("clicks-ava-hd").innerHTML = 0;
+    } else {
+        document.getElementById("clicks-ava-hd").innerHTML = o;
+    }
+}
+
+//Available Hab de lujo
+function pagarHDL() {
+    p = hdlAva - k;
+    
+    if (p <= 0) {
+        document.getElementById("clicks-ava-hdl").innerHTML = 0;
+    } else {
+        document.getElementById("clicks-ava-hdl").innerHTML = p;
+    }
+}
+
+//Available Hab Familiar
+function pagarHF() {
+    q = hfAva - l;
+    
+    if (q <= 0) {
+        document.getElementById("clicks-ava-hf").innerHTML = 0;
+    } else {
+        document.getElementById("clicks-ava-hf").innerHTML = q;
     }
 }
