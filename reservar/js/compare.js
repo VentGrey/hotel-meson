e3068ec350e16cd5dffeef240212b6f1e862339c
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-06-26 16:36:00 -0500

    Deleted file, I don't want to pull variables

--- reservar/js/compare.js
+++
@@ -1,8 +0,0 @@
-function compare() {
-    let start_date = document.getElementById("checkIn").value;
-    let end_date = document.getElementById("checkOut").value;
-
-    if ((new Date(start_date).getTime() < new Date(end_date).getTime())) {
-        // Código de comparación aquí adentro
-    }
-}
