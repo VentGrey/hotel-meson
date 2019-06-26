b1b4c0a575c9b094899a0ff70bb051494403fbdc
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-06-26 16:36:25 -0500

    Moved compare function here

--- reservar/js/counter.js
+++ reservar/js/counter.js
@@ -151,3 +151,12 @@ function pagarHF() {
         document.getElementById("clicks-ava-hf").innerHTML = q;
     }
 }*/
+
+function compare() {
+    let start_date = document.getElementById("checkIn").value;
+    let end_date = document.getElementById("checkOut").value;
+
+    if ((new Date(start_date).getTime() < new Date(end_date).getTime())) {
+        // Código de comparación aquí adentro
+    }
+}
