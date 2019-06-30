2ddb534f44f18fc1aae9d96e8065d4dc25fd8143
Adolf97 <ramirez.desanc@gmail.com>
2019-06-29 22:32:18 -0500

    carrusel para las otras habitaciones

--- js/galeria.js
+++ js/galeria.js
@@ -1,17 +1,75 @@
-var num = 1;
+//Carrusel Hab Sencilla
+var numhs = 1;
 
-function adelante() {
-  num++;
-  if (num > 6)
-    num = 1;
-  var imagen = document.getElementById("imagen");
-  imagen.src="images/imagen-hs-"+num+".jpeg";
+function adelantehs() {
+  numhs++;
+  if (numhs > 6)
+    numhs = 1;
+  var imagenhs = document.getElementById("imagenhs");
+  imagenhs.src="images/imagen-hs-"+numhs+".jpeg";
 }
 
-function atras() {
-  num--;
-  if (num < 1)
-    num = 6;
-  var imagen = document.getElementById("imagenh");
-  imagen.src="images/imagen-hs-"+num+".jpeg";
+function atrashs() {
+  numhs--;
+  if (numhs < 1)
+    numhs = 6;
+  var imagenhs = document.getElementById("imagenhs");
+  imagenhs.src="images/imagen-hs-"+numhs+".jpeg";
+}
+
+//Carrusel Hab Doble
+var numhd = 1;
+
+function adelantehd() {
+  numhd++;
+  if (numhd > 5)
+    numhd = 1;
+  var imagenhd = document.getElementById("imagenhd");
+  imagenhd.src="images/imagen-hd-"+numhd+".jpeg";
+}
+
+function atrashd() {
+  numhd--;
+  if (numhd < 1)
+    numhd = 5;
+  var imagenhd = document.getElementById("imagenhd");
+  imagenhd.src="images/imagen-hd-"+numhd+".jpeg";
+}
+
+//Carrusel Hab Triple
+var numht = 1;
+
+function adelanteht() {
+  numht++;
+  if (numht > 5)
+    numht = 1;
+  var imagenht = document.getElementById("imagenht");
+  imagenht.src="images/imagen-ht-"+numht+".jpeg";
+}
+
+function atrasht() {
+  numht--;
+  if (numht < 1)
+    numht = 5;
+  var imagenht = document.getElementById("imagenht");
+  imagenht.src="images/imagen-ht-"+numht+".jpeg";
+}
+
+//Carrusel Hab cuádruple
+var numhc = 1;
+
+function adelantehc() {
+  numhc++;
+  if (numhc > 5)
+    numhc = 1;
+  var imagenhc = document.getElementById("imagenhc");
+  imagenhc.src="images/imagen-hc-"+numhc+".jpeg";
+}
+
+function atrashc() {
+  numhc--;
+  if (numhc < 1)
+    numhc = 5;
+  var imagenhc = document.getElementById("imagenhc");
+  imagenhc.src="images/imagen-hc-"+numhc+".jpeg";
 }
