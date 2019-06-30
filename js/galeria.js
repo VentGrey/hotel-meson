92fe51b7a6bff9d6e61b0fddfeeeae309a2c619a
Adolf97 <ramirez.desanc@gmail.com>
2019-06-29 19:24:53 -0500

    made the respective changes

--- js/galeria.js
+++ js/galeria.js
@@ -2,16 +2,16 @@ var num = 1;
 
 function adelante() {
   num++;
-  if (num > 3)
+  if (num > 6)
     num = 1;
   var imagen = document.getElementById("imagen");
-  imagen.src="images/imagen-"+num+".jpg";
+  imagen.src="images/imagen-hs-"+num+".jpeg";
 }
 
 function atras() {
   num--;
   if (num < 1)
-    num = 3;
+    num = 6;
   var imagen = document.getElementById("imagen");
-  imagen.src="images/imagen-"+num+".jpg";
+  imagen.src="images/imagen-hs-"+num+".jpeg";
 }
