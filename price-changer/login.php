5109fba3161a7fbbaef70b7609a97dbf0a7696e9
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-04 23:17:04 -0500

    php summary

---
+++ price-changer/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Iniciar Sesión </title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div id="frm">
+            <form action="process.php" method="POST">
+                <p>
+                    <label>Usuario:</label>
+                    <input type="text" id="pass" name="pass" />
+                </p>
+                <p>
+                </p>
+            </form>
+        </div>
+    </body>
+</html>
