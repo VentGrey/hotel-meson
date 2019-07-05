95dc240c659c21c79799c42b6bbcd655195252e5
Adolf97 <ramirez.desanc@gmail.com>
2019-07-05 02:41:18 -0500

    indents

--- price-changer/index.php
+++ price-changer/index.php
@@ -13,19 +13,28 @@
 </head>
 
 <body>
-    <div></div>
     <div>
-        <nav class="navbar navbar-light navbar-expand-md shadow-sm d-flex justify-content-xl-center align-items-xl-center bounce animated navigation-clean" style="background-color: #380000;">
-            <div class="container"><a class="navbar-brand text-center shadow-sm d-xl-flex" href="#" style="font-family: Montserrat, sans-serif;color: #FAFAFA;">MESÓN DE JUAN VALLE</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-                <div
-                    class="collapse navbar-collapse" id="navcol-1">
-                    <ul class="nav navbar-nav ml-auto"></ul>
-            </div>
+      
     </div>
-    </nav>
+    <div>
+      <nav class="navbar navbar-light navbar-expand-md shadow-sm d-flex justify-content-xl-center align-items-xl-center bounce animated navigation-clean" style="background-color: #380000;">
+        <div class="container">
+          <a class="navbar-brand text-center shadow-sm d-xl-flex" href="#" style="font-family: Montserrat, sans-serif;color: #FAFAFA;">MESÓN DE JUAN VALLE</a>
+          <button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="nav navbar-nav ml-auto"></ul>
+          </div>
+        </div>
+      </nav>
     </div>
+    
     <h1 class="display-3 text-lowercase text-center">Cambiar Precios de las habitaciones.</h1>
     <p>Seleccione la habitación cuyo precio desea cambiar:</p>
+    
+    <!--Scripts-->
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
