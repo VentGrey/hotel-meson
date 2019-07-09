5566f3f9956239873cd495d5a60e3cbb3f8911ca
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-09 12:47:47 -0500

    Hardcoded price "just in case"

--- CreateCharge.php
+++ CreateCharge.php
@@ -4,11 +4,11 @@ require 'vendor/autoload.php';
 \Stripe\Stripe::setApiKey('sk_test_UqbYcqyb1MhZbYxq8XvuXn7h00imeKMLGE');
 
 $token = $_POST["stripeToken"];
-$price = $_POST["pesos"];
+
 
 $charge = \Stripe\Charge::create([
-    "amount" => $price,
-    "currency" => "usd",
+    "amount" => 1200,
+    "currency" => "mxn",
     "description" => "Pago en mi tienda...",
     "source" => $token
 ]);
