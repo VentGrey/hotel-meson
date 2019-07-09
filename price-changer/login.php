8adfa0ab9baececf7667dd2cd6ee6a7d171b0261
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-09 02:19:01 -0500

    Someone fvcked up this file. I've fixed it in this commit.

--- price-changer/login.php
+++ price-changer/login.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> Counter con input type="number" </title>
+        <title> Iniciar Sesión </title>
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
         <div id="frm">
-
-            <form name="f1" action="<?=$_SERVER["PHP_SELF"]?>" method="post">
-              <input type="number" min="0" max="11" name="num">
-              <input type="submit" name="" value="Enviar">
+            <form action="process.php" method="POST">
+                <p>
+                    <label>Usuario:</label>
+                    <input type="text" id="user" name="user" />
+                </p>
+                <p>
+                    <label>Contraseña:</label>
+                    <input type="password" id="password" name="password" />
+                </p>
+                <p>
+                    <input type="submit" id="btn" value="login" />
+                </p>
             </form>
-
-            <?php
-            $num_hab = $_POST['num'];
-            $precio = 1200;
-            $total = $num_hab * $precio;
-            echo$total;
-            ?>
         </div>
     </body>
 </html>
