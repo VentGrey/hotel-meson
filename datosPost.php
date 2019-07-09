f871e0ee3b3097c2521deffad11e4fff23e42b18
Adolf97 <ramirez.desanc@gmail.com>
2019-07-09 15:07:04 -0500

    added html code from pago-exitoso in datosPost.php

--- datosPost.php
+++ datosPost.php
@@ -24,3 +24,33 @@
   }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <link rel="icon" type="imagen" href="images/icono-1.ico">
+  <title>Pago Exitoso</title>
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet">
+  <!-- Page styles -->
+  <link href="css/style-pago.css" rel="stylesheet">
+</head>
+
+<body>
+
+<!--Mensaje-->
+<div class="container">
+  <div class="message">
+    <span>Gracias por reservar con nosotros</span>
+    <span>Tu pago ha sido realizado con éxito :)</span>
+  </div>
+</div>
+<!--/.Mensaje-->
+
+</body>
+
+</html>
