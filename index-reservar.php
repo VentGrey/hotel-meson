4eed38af555cc7fe15640a513c3e1217cf7dcd11
Adolf97 <ramirez.desanc@gmail.com>
2019-07-09 19:10:40 -0500

    index-reservar

--- index-reservar.php
+++ index-reservar.php
@@ -273,10 +273,30 @@
       // Conectarse a una BD
       $db = new SQLite3('login.sqlite3');
 
-      $precio_habsen = 1200;
-      $precio_habdob = 1500;
-      $precio_habtri = 1800;
-      $precio_habcua = 2000;
+      //BD que servirá para manejar operaciones con los precios reales.
+      $forPrDB = new SQLite3('backend/db.sqlite3');
+      //Variables a las que se le asignarán los precios.
+      $precio_habsen; //Variable para el precio de la Sencilla.
+      $precio_habdob; //Variable para el precio de la Doble.
+      $precio_habtri; //Variable para el precio de la Triple.
+      $precio_habcua; //Variable para el precio de la Cuádruple.
+      //Consulta para agarrar los datos de la tabla de precios.
+      $statement = $new->query("SELECT * FROM main_roomtype");
+      while($row = $statement->fetchArray()) { //While para cada fila de tabla.
+        $row = (object) $row;
+        if($row->id == 1) { //If para habitación Sencilla.
+          $precio_habsen = $row->price; //Asignando el precio sencillo.
+        }
+        elseif($row->id == 2) { //If para habitación Doble.
+          $precio_habdob = $row->price; //Asignando el precio Doble.
+        }
+        elseif($row->id == 3) { //If para habitación Triple.
+          $precio_habtri = $row->price; //Asignando el precio Triple.
+        }
+        else { //Else que servirá para habitación Cuádruple.
+          $precio_habcua = $row->price; //Asignando el precio Cuádruple.
+        }
+      }
 
       $num_habsen = $_POST['num-habsen'];
       $num_habdob = $_POST['num-habdob'];
@@ -284,7 +304,7 @@
       $num_habcua = $_POST['num-habcua'];
 
       $total = (($precio_habsen*$num_habsen)+($precio_habdob*$num_habdob)+($precio_habtri*$num_habtri)+($precio_habcua*$num_habcua));
-      echo$total;
+      echo"Hola";
       ?>
     </form>
     <!--/.Counter-->
