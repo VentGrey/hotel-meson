c4387a7d69f97fc07f10a59510de22ff88dbaf1b
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-18 19:14:47 -0500

    Modified login

--- administracion/login.php
+++ administracion/login.php
@@ -1,24 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title> Iniciar Sesión </title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
-    <body>
-        <div id="frm">
-            <form action="process.php" method="POST">
-                <p>
-                    <label>Usuario:</label>
-                    <input type="text" id="user" name="user" />
-                </p>
-                <p>
-                    <label>Contraseña:</label>
-                    <input type="password" id="password" name="password" />
-                </p>
-                <p>
-                    <input type="submit" id="btn" value="login" />
-                </p>
-            </form>
-        </div>
-    </body>
-</html>
+<form name="login" action="loguear.php" method="POST">
+  <br>
+  <label> Nombre del usuario </label>
+  <input type="text" name="username" />
+  <br>
+  <label> Contraseña </label>
+  <input type="password" name="password" />
+  </section>
+  <br>
+  <input type="submit" value="Iniciar Sesion">
+</form>
